063cdbacb799c7d848f111c559aedef717aefa83
Agasi <agasihana29@gmail.com>
2023-11-29 17:03:28 +0700

    hw14 perbaikan

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import { AuthProvider } from "@/modules/context/authContext";
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
+
+  );
 }
+
